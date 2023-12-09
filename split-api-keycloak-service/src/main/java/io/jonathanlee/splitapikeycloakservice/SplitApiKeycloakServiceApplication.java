f7eb62ad50e5065d72a8b948ecc1d16853fad4e5
Jonathan Lee <jonathan.lee.devel@gmail.com>
2023-12-09 22:48:05 +0000

    feat(keycloak): Added Keycloak service from Spring Initializr

---
+++ split-api-keycloak-service/src/main/java/io/jonathanlee/splitapikeycloakservice/SplitApiKeycloakServiceApplication.java
@@ -0,0 +1,13 @@
+package io.jonathanlee.splitapikeycloakservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitApiKeycloakServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitApiKeycloakServiceApplication.class, args);
+	}
+
+}
