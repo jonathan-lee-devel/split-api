1a3dd5beecc810c23e9fed289e0dbd676da2756c
Jonathan Lee <jonathan.lee.devel@gmail.com>
2023-12-09 22:56:52 +0000

    feat(mail): Added mail service from Spring Initializr

---
+++ split-api-mail-service/src/main/java/io/jonathanlee/splitapimailservice/SplitApiMailServiceApplication.java
@@ -0,0 +1,13 @@
+package io.jonathanlee.splitapimailservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitApiMailServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitApiMailServiceApplication.class, args);
+	}
+
+}
---
+++ split-api-mail-service/src/test/java/io/jonathanlee/splitapimailservice/SplitApiMailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.jonathanlee.splitapimailservice;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class SplitApiMailServiceApplicationTests {
+
+	@Test
+	void assertTrue() {
+		Assertions.assertTrue(true);
+	}
+
+}
